b1b5e58b3dabf2b875ac0af90c7ad26df76fe20a
Loaye <jr.carstens00@gmail.com>
2017-06-22 10:44:32 -0700

    Adds handlebars template for class requirement, not using handlebars for my project

--- scripts/app.js
+++ scripts/app.js
@@ -3,23 +3,23 @@
 var articles = [];
 
 function Article (rawDataObj) {
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
   this.title = rawDataObj.title;
-  this.url = rawDataObj.url;
-  this.publishedOn = rawDataObj.publishedOn;
+  this.category = rawDataObj.category;
   this.body = rawDataObj.body;
+  this.publishedOn = rawDataObj.publishedOn;
 }
 
 Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
-  $newArticle.removeClass('template');
+  var template = $('#template').html();
 
-  if (!this.publishedOn) $newArticle.addClass('draft');
-  $newArticle.find('.byline a').attr('href', this.url);
-  $newArticle.find('.article-body').html(this.body);
-  $newArticle.find('.byline time').attr('pubdate', this.publishedOn);
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-  $newArticle.append('<hr>');
-  return $newArticle;
+  var renderTemplate = Handlebars.compile(template);
+
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+
+  return renderTemplate(this);
 };
 
 rawData.sort(function(a,b) {
@@ -30,10 +30,6 @@ rawData.forEach(function(articleObject) {
   articles.push(new Article(articleObject));
 });
 
-articles.forEach(function(article) {
+articles.forEach(function(article){
   $('#articles').append(article.toHtml());
-});
-
-$('span').on('click', function(){
-  $('nav ul').toggleClass('show');
 });
