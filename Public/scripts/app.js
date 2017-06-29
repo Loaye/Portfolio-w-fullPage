a7df7fadb52a616be4dc6466b2f397ba25ee68a8
Loaye <jr.carstens00@gmail.com>
2017-06-29 11:28:10 -0700

    adds image and paragraph swapper on the about me age

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -4,3 +4,21 @@
 $('span').on('click', function(){
   $('nav ul').toggleClass('show');
 });
+
+//Swaps image on "About Me" page
+$('.ranger').on('click', function() {
+  $('.ranger').fadeOut(400, function(){
+    $('.portrait').fadeIn(400, function(){
+      $('.portrait').removeClass('.invisible');
+    });
+  });
+});
+
+//Swaps paragraphs on "About Me" page
+$('.summary').on('click', function() {
+  $('.summary').fadeOut(400, function(){
+    $('.skills').fadeIn(400, function(){
+      $('.skills').removeClass('.invisible');
+    });
+  });
+});
