2f3d74e22b3be8f85501f96ff0758bef8d286de4
Loaye <jr.carstens00@gmail.com>
2017-06-30 08:28:47 -0700

    adds controllers

---
+++ Public/scripts/controllers/projectController.js
@@ -0,0 +1,12 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+  const projectController = {};
+
+  projectController.index = () => {
+    app.Project.fetchAll(app.projectView.initIndexPage);
+  }
+
+  module.projectController = projectController
+})(app);
